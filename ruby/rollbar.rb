726704c9f6222a0f4021e80072452dbc5b910200
Tom Kruijsen <tom@tomkruijsen.nl>
2017-02-20 14:48:22 +0100

    Only enable Rollbar in production environments

--- ruby/rollbar.rb
+++ ruby/rollbar.rb
@@ -1,10 +1,11 @@
-Rollbar.configure do |config|
-  config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN')
-  config.enabled = false if %w(development test).include?(Rails.env)
-  config.environment = ENV.fetch('ROLLBAR_ENV', Rails.env)
-  config.exception_level_filters.merge!(
-    'ActiveRecord::RecordNotFound' => 'ignore',
-    'ActionController::RoutingError' => 'ignore',
-    'ActionController::UnknownFormat' => 'ignore'
-  )
+if Rails.env.production?
+  Rollbar.configure do |config|
+    config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN')
+    config.environment = ENV.fetch('ROLLBAR_ENV', Rails.env)
+    config.exception_level_filters.merge!(
+      'ActiveRecord::RecordNotFound' => 'ignore',
+      'ActionController::RoutingError' => 'ignore',
+      'ActionController::UnknownFormat' => 'ignore'
+    )
+  end
 end
