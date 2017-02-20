86b9e4e3524728766aff69c3d532f6991692f25d
Tom Kruijsen <tom@tomkruijsen.nl>
2017-02-20 14:33:14 +0100

    Don't default to nil for rollbar access token

--- ruby/rollbar.rb
+++ ruby/rollbar.rb
@@ -1,5 +1,5 @@
 Rollbar.configure do |config|
-  config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN', nil)
+  config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN')
   config.enabled = false if %w(development test).include?(Rails.env)
   config.environment = ENV.fetch('ROLLBAR_ENV', Rails.env)
   config.exception_level_filters.merge!(
